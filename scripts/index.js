a7a21d1d2f371a12ab4fd29e1bc125f5f6413c2c
Alberto Baroso <alberto.baroso886@edu.unito.it>
2021-01-31 16:01:49 +0100

    Updated skills, renamed files

--- scripts/index.js
+++ scripts/index.js
@@ -1,11 +1,11 @@
-$(function() {
+$(function () {
 
     /*/
      *      Utility
     /*/
 
     /*/ Open/Close Navigation Bar /*/
-    $("#navbar-toggle").on("click", function() {
+    $("#navbar-toggle").on("click", function () {
         $("#main-nav").toggleClass("show");
     });
 
@@ -19,38 +19,37 @@ $(function() {
      *      Animations
     /*/
 
-    /*/ Links Animation + Page scrolling /*/
-    $(".nav-link").on("click", function() {
+    /*/ Link Animations + Page scrolling /*/
+    $(".nav-link").on("click", function () {
 
         /*/ Set clicked link as the only active link /*/
         $(".nav-link").removeClass("active");
         $(this).addClass("active");
 
         /*/ Close the link menu if it was open /*/
-        setTimeout(function() {
-           $("#main-nav.show").removeClass("show");
+        setTimeout(function () {
+            $("#main-nav.show").removeClass("show");
         }, 250);
 
         /*/ Stop animations on links /*/
         animateLinks = false;
         $(this).stop(true);
-        
+
         /*/ Scroll animation to target /*/
         $("html, body").animate({
             scrollTop: $(`#${$(this).data("target")}-section`).offset().top
         }, 1200);
 
         /*/ Re-Enable animations on links /*/
-        setTimeout(function() {
+        setTimeout(function () {
             animateLinks = true;
         }, 1200);
 
         return false;
-
     });
 
     /*/ Scroll animation to target /*/
-    $(".footer-link").on("click", function() {
+    $(".footer-link").on("click", function () {
 
         $("html, body").animate({
             scrollTop: $(`#${$(this).data("target")}-section`).offset().top
@@ -75,7 +74,7 @@ function checkAnimations(windowTop) {
     let windowHeight = $window.height();
 
     /*/ Check for every ".animate" element if it is in viewport /*/
-    $.each($toAnimate, function() {
+    $.each($toAnimate, function () {
 
         let $element = $(this);
         let elementTop = $element.offset().top;
@@ -83,15 +82,15 @@ function checkAnimations(windowTop) {
         let elementBottom = (elementTop + elementHeight);
 
         if ((elementBottom >= windowTop) && (elementTop <= windowTop + windowHeight)) {
-            if($element.attr("id") === "header-text" && $("#header-text").hasClass("animate")) {
+            if ($element.attr("id") === "header-text" && $("#header-text").hasClass("animate")) {
 
                 /*/ Custom titles animation /*/
                 setTimeout(writeTitles, 1500);
 
-            } else if($element.hasClass("languages-container") && $element.hasClass("animate")) {
+            } else if ($element.hasClass("languages-container") && $element.hasClass("animate")) {
 
                 /*/ Custom languages animation /*/
-                setTimeout(function() {
+                setTimeout(function () {
                     knowledgeAnimation($element);
                 }, 1000);
 
@@ -118,7 +117,7 @@ function checkScroll() {
     }
 
     /*/ Link Animations /*/
-    if(animateLinks) {
+    if (animateLinks) {
         checkSection(scrollTop);
     }
 
@@ -141,8 +140,8 @@ function checkSection(scrollTop) {
 
             /*/ Change Active Link /*/
             $(".nav-link").removeClass("active");
-            $(".nav-link").each(function() {
-                if($(this).data("target") === id.replace("-section", '')) {
+            $(".nav-link").each(function () {
+                if ($(this).data("target") === id.replace("-section", '')) {
                     $(this).addClass("active");
                 }
             });
@@ -161,7 +160,7 @@ function writeTitles() {
     let count = 0;
     let titles;
 
-    if(lang === "it") {
+    if (lang === "it") {
         titles = ["Sviluppatore Front End", "Programmatore Software", "Sviluppatore Back End"];
     } else {
         titles = ["Front End Developer", "Software Programmer", "Back End Developer"];
@@ -171,14 +170,14 @@ function writeTitles() {
 
         let size = profession.text();
 
-        setTimeout(function() {
+        setTimeout(function () {
 
             profession.text(profession.text().slice(0, profession.text().length - 1));
 
-            if(profession.text() == "") {
-                setTimeout(function() {
+            if (profession.text() === "") {
+                setTimeout(function () {
                     count = 0;
-                    setTimeout(writeWord, 200);
+                    setTimeout(writeWord, 600);
                 }, size * 50);
             } else {
                 deleteWord();
@@ -192,11 +191,11 @@ function writeTitles() {
 
         profession.text(profession.text() + titles[index][count++]);
 
-        if(count == titles[index].length) {
+        if (count == titles[index].length) {
             index = (index + 1) % titles.length;
             setTimeout(deleteWord, 1200);
         } else {
-            setTimeout(writeWord, 100);
+            setTimeout(writeWord, 95);
         }
 
     }
@@ -208,55 +207,65 @@ function writeTitles() {
 
 function knowledgeAnimation(self) {
 
-    self.find(".language-row").each(function(j) {
+    $(".languages-category, .knowledge-legend").animate({
+        opacity: 1
+    });
 
-        let experienceText = $(this).find(".experience-text");
-        let experience = $(this).find(".experience");
-        let languageName = $(this).find(".language-name");
-        let level = $(this).find(".language-level");
-        let start = experience.data("start");
-        let years;
+    self.find(".language").each(function (j) {
 
-        if(start === undefined) {
-            years = experience.data("years");
-        } else {
-            let today = new Date();
-            years = today.getFullYear() - start;
-        }
-
-        $(".languages-category").animate({
-            opacity: 1
-        });
-
-        $(languageName).delay(100 + j * 100).animate({
+        $(this).delay(150 + j * 100).animate({
             opacity: 1
         }, 500);
 
-        for(let i=0; i<7; i++) {
-
-            let diamond = $(`<span class="diamond"></span>`);
-            level.append(diamond);
-
-            $(diamond).delay(400 + j * 100).animate({
-                opacity: 1
-            }, 300);
-
-            if(i < years) {
-                $(diamond).delay(j * 100 + (i + j * 7) * 75).animate({
-                    backgroundColor: "#0254d2"
-                }, 300);
-            }
-
-        }
-
-        if(years > 7) {
-            years = "7+";
-        }
-
-        experience.html(years);
-        $(experienceText).delay(1200 + j * 100 + (j+1) * 7 * 75).animate({
-            opacity: 1
-        }, 400);
+        //     let experienceText = $(this).find(".experience-text");
+        //     let experience = $(this).find(".experience");
+        //     let languageName = $(this).find(".language-name");
+        //     let languageLevel = $(this).find(".language-level");
+        //     let start = experience.data("start");
+        //     let years;
+        //
+        //     if (start === undefined) {
+        //         years = experience.data("years");
+        //     } else {
+        //         let today = new Date();
+        //         years = today.getFullYear() - start;
+        //     }
+        //
+        //
+        //
+        //     $(languageName).delay(100 + j * 100).animate({
+        //         opacity: 1
+        //     }, 500);
+        //
+        //     $(languageLevel).find(".diamond").delay(100 + j * 100).animate({
+        //         opacity: 1
+        //     }, 500);
+        //
+        //     for (let i = 0; i < 7; i++) {
+        //
+        //         let diamond = $(`<span class="diamond"></span>`);
+        //         languageLevel.append(diamond);
+        //
+        //         $(diamond).delay(400 + j * 100).animate({
+        //             opacity: 1
+        //         }, 300);
+        //
+        //         if (i < years) {
+        //             $(diamond).delay(j * 100 + (i + j * 7) * 75).animate({
+        //                 backgroundColor: "#0254d2"
+        //             }, 300);
+        //         }
+        //
+        //     }
+        //
+        //     if (years > 7) {
+        //         years = "7+";
+        //     }
+        //
+        //     experience.html(years);
+        //     $(experienceText).delay(1200 + j * 100 + (j + 1) * 7 * 75).animate({
+        //         opacity: 1
+        //     }, 400);
 
     });
 
@@ -264,22 +273,22 @@ function knowledgeAnimation(self) {
 
 
 /*/ Show/Hide Skill Descriptions /*/
-$(".skill-container").on("click", function() {
+$(".skill-container").on("click", function () {
 
     let skill = $(this).data("skill");
     let hidden = true;
 
-    $(".skill-description").each(function() {
-        if($(this).data("skill") == skill && $(this).is(":visible")) {
+    $(".skill-description").each(function () {
+        if ($(this).data("skill") == skill && $(this).is(":visible")) {
             hidden = false;
         }
     });
 
     $(".skill-description").slideUp();
 
-    if(hidden) {
-        $(".skill-description").each(function() {
-            if($(this).data("skill") == skill) {
+    if (hidden) {
+        $(".skill-description").each(function () {
+            if ($(this).data("skill") == skill) {
                 $(this).slideDown();
             }
         });
@@ -288,7 +297,7 @@ $(".skill-container").on("click", function() {
 });
 
 /*/ Back to Top Link Animation /*/
-$("#top-button").on("click", function() {
+$("#top-button").on("click", function () {
 
     $("html, body").animate({
         scrollTop: 0
